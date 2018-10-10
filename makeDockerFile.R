c4bcb456ca356bd84e1f2e4779fccc31abd6b1ba
pokyah <thomas.goossens@protonmail.com>
2018-10-10 15:08:57 +0200

    adding R source file

---
+++ makeDockerFile.R
@@ -0,0 +1,6 @@
+
+dockerfile_object <- dockerfile(from = file.path("tuto.Rmd"),
+  silent = TRUE,
+  cmd = CMD_Render("tuto.Rmd"))
+print(dockerfile_object)
+write(dockerfile_object, file = "./docker/Dockerfile")
